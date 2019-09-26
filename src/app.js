dccca6c2269b2896c1009215a9a49faccfc1e6e0
Matheus Soares <m.maciel3@hotmail.com>
2019-09-26 01:33:23 -0300

    Changing from standalone app to Express API

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
 const businessSearch = require('./businessSearch.js');
 const businessReview = require('./businessReview.js');
+const envVariables = require('../env.json');
 
-async function logTop5Shops() {
-    let top5Shops = await businessSearch.getTop5IceCreamShopsInAlpharetta()
-        .then(response => businessReview.getBusinessesWithReviews(response));
+const express = require('express');
+const app = express();
+const port = 3000;
 
-    console.log(top5Shops);
-}
+app.get('/top5', async (req, res) => {
+    try {
+        const top5Shops = await businessSearch.getTop5IceCreamShopsInAlpharetta()
+            .then(response => businessReview.getBusinessesWithReviews(response));
 
-logTop5Shops();
+        res.status(200).send(top5Shops);
+    } catch (error) {
+        console.log('Error when processing request:', error);
+        res.status(500).send('Unfortunately, we cannot complete your request at this time due to an internal server error.');
+    }
+});
+
+app.listen(port, () => {
+    if (!envVariables.apiKey) {
+        const errorMessage = 'ERROR: missing required API key. Your Yelp Fusion API key must be ' +
+            'placed in a file called \"env.json\" located in the root of the project. ' +
+            'This JSON file must contain a property called "apiKey" whose value must be your API key.';
+
+        console.error(errorMessage);
+        console.error('Aborting server startup.');
+
+        throw new Error(errorMessage);
+    }
+
+    console.log(`Server is running on port ${port}`)
+});
