4584965b4700e40337ed48bad3795cc07195fb1d
Matheus Soares <m.maciel3@hotmail.com>
2019-09-29 21:05:27 -0300

    Adding more assertions to the integration test - we now check that each of the businesses returned has a name, an address, a review and a reviewer, and that all of those are not empty

--- test/integration/api-test.spec.js
+++ test/integration/api-test.spec.js
@@ -1,6 +1,6 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const should = chai.should();
+const expect = chai.expect;
 
 const app = require('../../src/app');
 
@@ -10,10 +10,20 @@ describe('Caling the API', () => {
     it('should return HTTP 200 and body with 5 icecream shops', (done) => {
         chai.request(app.server)
             .get('/top5')
-            .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.be.a('array');
-                res.body.length.should.be.equal(5);
+            .end((err, response) => {
+                expect(response.status).to.be.equal(200);
+                expect(response.body).to.be.an('array').with.lengthOf(5);
+
+                let shops = response.body;
+                shops.forEach((shop) => {
+                    expect(shop).to.have.keys(['name', 'address', 'review', 'reviewer']);
+                    expect(Object.keys(shop).length).to.equal(4);
+                    expect(shop.name).to.not.be.empty;
+                    expect(shop.address).to.not.be.empty;
+                    expect(shop.review).to.not.be.empty;
+                    expect(shop.reviewer).to.not.be.empty;
+                });
+
                 done();
             });
     });
